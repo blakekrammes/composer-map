925f96abb2339617f9bb2506b0d2261ab39cf0ca
Blake Krammes <blake.c.krammes@gmail.com>
2018-12-19 10:35:28 -0800

    Remove hovering functionality for now

--- app/main.js
+++ app/main.js
@@ -64,7 +64,9 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/geometry/P
         composers_1.default.forEach(function (composer) {
             if (intYear < composer.birth || intYear > composer.death) {
                 graphicsLayer.graphics.remove(window[composer.name + 'Graphic']);
+                graphicsLayer.graphics.remove(window[composer.name + 'enlargedGraphic']);
                 composer.isDisplaying = false;
+                composer.isUpdatedGraphicDisplaying = false;
             }
         });
     }, 100);
@@ -77,62 +79,68 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/geometry/P
             document.getElementById('viewDiv').style.cursor = 'default';
         }
     }
-    function changeImgSize(res) {
-        year = document.getElementById('year').value;
-        var intYear = parseInt(year, 10);
-        if (res.results.length > 0) {
-            composers_1.default.forEach(function (composer) {
-                if (intYear >= composer.birth && intYear <= composer.death) {
-                    if (composer.isUpdatedGraphicDisplaying !== true
-                        && res.results[0].graphic.geometry.latitude === composer.coordinates.latitude
-                        && res.results[0].graphic.geometry.longitude === composer.coordinates.longitude) {
-                        var updatedMarker = new PictureMarkerSymbol({
-                            url: composer.url,
-                            width: "50px",
-                            height: "50px"
-                        });
-                        var point = new Point({
-                            longitude: composer.coordinates.longitude,
-                            latitude: composer.coordinates.latitude
-                        });
-                        var popupTemplate = new PopupTemplate({
-                            title: composer.popupTitle,
-                            content: composer.popupContent
-                        });
-                        window[composer.name + 'enlargedGraphic'] = new Graphic({
-                            geometry: point,
-                            symbol: updatedMarker,
-                            popupTemplate: popupTemplate
-                        });
-                        // add the new, hovered graphic to the view
-                        graphicsLayer.graphics.add(window[composer.name + 'enlargedGraphic']);
-                        composer.isUpdatedGraphicDisplaying = true;
-                        // remove the original graphic with a bit of delay for a smooth transition 
-                        setTimeout(function () {
-                            if (composer.isUpdatedGraphicDisplaying === true) {
-                                graphicsLayer.graphics.remove(window[composer.name + 'Graphic']);
-                                composer.isDisplaying = false;
-                            }
-                        }, 250);
-                    }
-                }
-            });
-        }
-        else {
-            composers_1.default.forEach(function (composer) {
-                if (intYear >= composer.birth && intYear <= composer.death && window[composer.name + 'enlargedGraphic']
-                    && composer.isUpdatedGraphicDisplaying !== false) {
-                    console.log(graphicsLayer.graphics);
-                    setTimeout(function () {
-                        graphicsLayer.graphics.remove(window[composer.name + 'enlargedGraphic']);
-                        composer.isUpdatedGraphicDisplaying = false;
-                    }, 30);
-                    graphicsLayer.graphics.add(window[composer.name + 'Graphic']);
-                    composer.isDisplaying = true;
-                }
-            });
-        }
-    }
+    // function changeImgSize(res) {
+    //     year = (<HTMLOutputElement>document.getElementById('year')).value;
+    //     let intYear = parseInt(year, 10);
+    //     if (res.results.length > 0) {
+    //         composers.forEach(composer => {
+    //             if (intYear >= composer.birth && intYear <= composer.death) {
+    //                 if (composer.isUpdatedGraphicDisplaying !== true
+    //                     && res.results[0].graphic.geometry.latitude === composer.coordinates.latitude 
+    //                     && res.results[0].graphic.geometry.longitude === composer.coordinates.longitude) {
+    //                     let updatedMarker = new PictureMarkerSymbol({
+    //                         url: composer.url,
+    //                         width: "50px",
+    //                         height: "50px"
+    //                     });
+    //                     let point = new Point({
+    //                         longitude: composer.coordinates.longitude,
+    //                         latitude: composer.coordinates.latitude
+    //                     });
+    //                     let popupTemplate = new PopupTemplate({
+    //                         title: composer.popupTitle,
+    //                         content: composer.popupContent
+    //                     });
+    //                     window[composer.name+'enlargedGraphic'] = new Graphic({
+    //                         geometry: point,
+    //                         symbol: updatedMarker,
+    //                         popupTemplate: popupTemplate
+    //                     });
+    //                     // add the new, hovered graphic to the view
+    //                     graphicsLayer.graphics.add(window[composer.name+'enlargedGraphic']);
+    //                     composer.isUpdatedGraphicDisplaying = true;
+    //                     // remove the original graphic with a bit of delay for a smooth transition 
+    //                     setTimeout(function(){
+    //                         if (composer.isUpdatedGraphicDisplaying === true) {
+    //                             graphicsLayer.graphics.remove(window[composer.name+'Graphic']);
+    //                             composer.isDisplaying = false;
+    //                         }
+    //                     }, 250)
+    //                 }
+    //             }
+    //         });
+    //     }
+    //     else {
+    //         composers.forEach(composer => {
+    //             if (intYear >= composer.birth && intYear <= composer.death && window[composer.name+'enlargedGraphic'] 
+    //                 && composer.isUpdatedGraphicDisplaying !== false) {
+    //                 console.log(graphicsLayer.graphics)
+    //                 composer.isUpdatedGraphicDisplaying = false;
+    //                 setTimeout(function(){
+    //                     graphicsLayer.graphics.remove(window[composer.name+'enlargedGraphic']);
+    //                 }, 30);
+    //                 // setTimeout(function() {
+    //                 if (window[composer.name+'Graphic']) {}
+    //                 // if (composer.isUpdatedGraphicDisplaying === false) {
+    //                     else {
+    //                     graphicsLayer.graphics.add(window[composer.name+'Graphic']);
+    //                     composer.isDisplaying = true;
+    //                     }
+    //                 // },1)
+    //             }
+    //         });
+    //     }
+    // }
     view.on("pointer-move", function (event) {
         var screenPoint = {
             x: event.x,
@@ -141,7 +149,7 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/geometry/P
         view.hitTest(screenPoint)
             .then(function (response) {
             changeCursor(response);
-            changeImgSize(response);
+            // changeImgSize(response);
         });
     });
     var inputDiv = document.getElementsByClassName('input-div')[0];
--- app/main.ts
+++ app/main.ts
@@ -45,7 +45,7 @@ window.setInterval(() => {
         if (intYear >= composer.birth && intYear <= composer.death) {
             if (composer.isDisplaying !== true && composer.isUpdatedGraphicDisplaying !== true) {
 
-                graphicsLayer.graphics.items.forEach(graphic => {
+                graphicsLayer.graphics.items.forEach(function(graphic:any) {
                     if (graphic.geometry.longitude === composer.coordinates.longitude && graphic.geometry.latitude === composer.coordinates.latitude) {
                         composer.coordinates.longitude = composer.coordinates.longitude + .08;
                     }
@@ -81,7 +81,9 @@ window.setInterval(() => {
     composers.forEach(composer => {
         if (intYear < composer.birth || intYear > composer.death) {
             graphicsLayer.graphics.remove(window[composer.name+'Graphic']);
+            graphicsLayer.graphics.remove(window[composer.name+'enlargedGraphic']);
             composer.isDisplaying = false;
+            composer.isUpdatedGraphicDisplaying = false;
         }
     });
 
@@ -89,7 +91,7 @@ window.setInterval(() => {
 
 map.add(graphicsLayer);
 
-function changeCursor(res) {
+function changeCursor(res:any) {
     if (res.results.length > 0) {
         document.getElementById('viewDiv').style.cursor = 'pointer';
     }
@@ -98,68 +100,79 @@ function changeCursor(res) {
     }
 }
 
-function changeImgSize(res) {
+// function changeImgSize(res) {
 
-    year = (<HTMLOutputElement>document.getElementById('year')).value;
+//     year = (<HTMLOutputElement>document.getElementById('year')).value;
 
-    let intYear = parseInt(year, 10);
+//     let intYear = parseInt(year, 10);
 
-    if (res.results.length > 0) {
-        composers.forEach(composer => {
-            if (intYear >= composer.birth && intYear <= composer.death) {
-                if (composer.isUpdatedGraphicDisplaying !== true
-                    && res.results[0].graphic.geometry.latitude === composer.coordinates.latitude 
-                    && res.results[0].graphic.geometry.longitude === composer.coordinates.longitude) {
-                    let updatedMarker = new PictureMarkerSymbol({
-                        url: composer.url,
-                        width: "50px",
-                        height: "50px"
-                    });
-                    let point = new Point({
-                        longitude: composer.coordinates.longitude,
-                        latitude: composer.coordinates.latitude
-                    });
-                    let popupTemplate = new PopupTemplate({
-                        title: composer.popupTitle,
-                        content: composer.popupContent
-                    });
-                    window[composer.name+'enlargedGraphic'] = new Graphic({
-                        geometry: point,
-                        symbol: updatedMarker,
-                        popupTemplate: popupTemplate
-                    });
-                    // add the new, hovered graphic to the view
-                    graphicsLayer.graphics.add(window[composer.name+'enlargedGraphic']);
-                    composer.isUpdatedGraphicDisplaying = true;
-                    // remove the original graphic with a bit of delay for a smooth transition 
-                    setTimeout(function(){
-                        if (composer.isUpdatedGraphicDisplaying === true) {
-                            graphicsLayer.graphics.remove(window[composer.name+'Graphic']);
-                            composer.isDisplaying = false;
-                        }
-                    }, 250)
+//     if (res.results.length > 0) {
+//         composers.forEach(composer => {
+//             if (intYear >= composer.birth && intYear <= composer.death) {
+//                 if (composer.isUpdatedGraphicDisplaying !== true
+//                     && res.results[0].graphic.geometry.latitude === composer.coordinates.latitude 
+//                     && res.results[0].graphic.geometry.longitude === composer.coordinates.longitude) {
+
+//                     let updatedMarker = new PictureMarkerSymbol({
+//                         url: composer.url,
+//                         width: "50px",
+//                         height: "50px"
+//                     });
+//                     let point = new Point({
+//                         longitude: composer.coordinates.longitude,
+//                         latitude: composer.coordinates.latitude
+//                     });
+//                     let popupTemplate = new PopupTemplate({
+//                         title: composer.popupTitle,
+//                         content: composer.popupContent
+//                     });
+//                     window[composer.name+'enlargedGraphic'] = new Graphic({
+//                         geometry: point,
+//                         symbol: updatedMarker,
+//                         popupTemplate: popupTemplate
+//                     });
+//                     // add the new, hovered graphic to the view
+                
+//                     graphicsLayer.graphics.add(window[composer.name+'enlargedGraphic']);
+//                     composer.isUpdatedGraphicDisplaying = true;
                     
-                }
-            }
-        });
-    }
-    else {
-        composers.forEach(composer => {
-            if (intYear >= composer.birth && intYear <= composer.death && window[composer.name+'enlargedGraphic'] 
-                && composer.isUpdatedGraphicDisplaying !== false) {
-                console.log(graphicsLayer.graphics)
+//                     // remove the original graphic with a bit of delay for a smooth transition 
+//                     setTimeout(function(){
+//                         if (composer.isUpdatedGraphicDisplaying === true) {
+//                             graphicsLayer.graphics.remove(window[composer.name+'Graphic']);
+//                             composer.isDisplaying = false;
+//                         }
+//                     }, 250)
+                    
+//                 }
+//             }
+//         });
+//     }
+//     else {
+//         composers.forEach(composer => {
+//             if (intYear >= composer.birth && intYear <= composer.death && window[composer.name+'enlargedGraphic'] 
+//                 && composer.isUpdatedGraphicDisplaying !== false) {
+//                 console.log(graphicsLayer.graphics)
+
+//                 composer.isUpdatedGraphicDisplaying = false;
+//                 setTimeout(function(){
+//                     graphicsLayer.graphics.remove(window[composer.name+'enlargedGraphic']);
+//                 }, 30);
+//                 // setTimeout(function() {
+//                 if (window[composer.name+'Graphic']) {}
+//                 // if (composer.isUpdatedGraphicDisplaying === false) {
+//                     else {
+//                     graphicsLayer.graphics.add(window[composer.name+'Graphic']);
+//                     composer.isDisplaying = true;
+//                     }
                 
+                    
+//                 // },1)
                 
-                setTimeout(function(){
-                    graphicsLayer.graphics.remove(window[composer.name+'enlargedGraphic']);
-                    composer.isUpdatedGraphicDisplaying = false;
-                }, 30);
-                graphicsLayer.graphics.add(window[composer.name+'Graphic']);
-                composer.isDisplaying = true;
-            }
-        });
-    }
-}
+//             }
+//         });
+//     }
+// }
 
 view.on("pointer-move", function(event){
 
@@ -171,7 +184,7 @@ view.on("pointer-move", function(event){
     view.hitTest(screenPoint)
         .then(function(response) {
             changeCursor(response);
-            changeImgSize(response);
+            // changeImgSize(response);
         });
   });
 
